592633a88b02c314f3ff0946be7f993bc96aeb69
Andrew Cloete <andrew.cloete@spatialedge.co.za>
2024-07-03 11:52:44 +0200

    Increase TAPPING_TERM to 200

--- keyboards/crkbd/keymaps/andrew/config.h
+++ keyboards/crkbd/keymaps/andrew/config.h
@@ -24,12 +24,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* Select hand configuration */
 
-// #define MASTER_LEFT
-#define MASTER_RIGHT
+#define MASTER_LEFT
+// #define MASTER_RIGHT
 // #define EE_HANDS
 
 //#define QUICK_TAP_TERM 0
-#define TAPPING_TERM 150
+#define TAPPING_TERM 200
 #define SPLIT_USB_DETECT
 
 #ifdef RGBLIGHT_ENABLE
