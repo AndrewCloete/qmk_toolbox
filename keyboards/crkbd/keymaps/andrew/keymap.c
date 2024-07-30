cf8c4b5652473d5ec4548612932a90236d4d76f6
Andrew Cloete <andrew.cloete@spatialedge.co.za>
2024-07-30 09:07:22 +0200

    LALT_T top left. Now more alpha modifiers

--- keyboards/crkbd/keymaps/andrew/keymap.c
+++ keyboards/crkbd/keymaps/andrew/keymap.c
@@ -64,11 +64,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT(
   //├────────┼────────┼────────┼────────┼────────┼────────┐                                  ┌────────┼────────┼────────┼────────┼────────┼────────┤
-     LALT_T(KC_TAB),KC_Q ,KC_W ,KC_E    ,KC_R    ,KC_T                                       ,KC_Y    ,KC_U    ,KC_I    ,KC_O    ,KC_P    ,LT(_SYMB, KC_EQL),
+     LALT_T(KC_TAB),KC_Q ,KC_W ,KC_E    ,KC_R    ,KC_T                                       ,KC_Y    ,KC_U    ,KC_I    ,KC_O    ,KC_P    ,LALT_T(KC_EQL),
   //├────────┼────────┼────────┼────────┼────────┼────────┤                                  ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_NAESC ,KC_A  ,KC_S    ,KC_D    ,KC_F   ,KC_G                                       ,KC_H    ,KC_J    ,KC_K    ,KC_L    ,LT(_NAV, KC_SCLN) ,KC_QUOT,
   //├────────┼────────┼────────┼────────┼────────┼────────┐                                  ┌────────┼────────┼────────┼────────┼────────┼────────┤
-     SYM_L   ,KC_Z    ,KC_X    ,KC_C    ,KC_V    ,KC_B                                       ,KC_N    ,LALT_T(KC_M)    ,KC_COMM ,KC_DOT  ,KC_SLSH , KC_BSLS,
+     SYM_L   ,KC_Z    ,KC_X    ,KC_C    ,KC_V    ,KC_B                                       ,KC_N    ,KC_M    ,KC_COMM ,KC_DOT  ,KC_SLSH , LT(_SYMB, KC_BSLS),
   //├───────┼────────┼────────┼────────┼────┬───┴────┬───┼────────┼────────┤        ├────────┼────────┼───┬────┴───┬────┼────────┼────────┼────────┤
                                              KC_LCTL ,    KC_LSFT ,KC_LWIN ,        KC_BSPC  ,KC_SPC  ,    KC_ENT
   //                                        └────────┘   └────────┴────────┘       └────────┴────────┘   └────────┘
